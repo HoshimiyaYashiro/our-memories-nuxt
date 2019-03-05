9817cd7ad65a91ae4307720fc20de4d7caac58b4
Hoshimiya Yashiro <khuongnqdev@gmail.com>
2019-03-06 05:55:41 +0700

    Commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,37 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        our-memories
-      </h1>
-      <h2 class="subtitle">
-        My finest Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+  <section class="root">
+    <div class="container page-container">
+      <div class="page-header">
+        <div class="theme-image-container"></div>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
+  components: {}
 }
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Dancing+Script');
+@import 'assets/sass/common-styles.sass';
+.theme-image-container {
+  background-image: url('/bg/bg-top.png');
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center top;
+  height: 120px;
+}
+.title-container {
   text-align: center;
 }
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.wedding-title {
+  font-family: 'Dancing Script', cursive;
+  font-size: 4.5rem;
+  font-style: normal;
+  font-weight: normal;
+  color: #685850;
 }
 </style>
